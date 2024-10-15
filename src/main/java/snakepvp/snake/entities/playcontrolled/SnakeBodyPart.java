d9887b35e115616a0aca9af839d20accde12bf2a
D-Jordy <kaaljordy@gmail.com>
2024-10-15 14:48:34 +0200

    Game over if you hit yourself

--- src/main/java/snakepvp/snake/entities/playcontrolled/SnakeBodyPart.java
+++ src/main/java/snakepvp/snake/entities/playcontrolled/SnakeBodyPart.java
@@ -2,10 +2,10 @@ package snakepvp.snake.entities.playcontrolled;
 
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 
-public abstract class SnakeBodyPart extends DynamicSpriteEntity
-{
+public abstract class SnakeBodyPart extends DynamicSpriteEntity implements Collider {
     protected double direction;
 
     protected SnakeBodyPart(String resource, Coordinate2D initialLocation, Size size, double direction, double speed) {
@@ -16,27 +16,32 @@ public abstract class SnakeBodyPart extends DynamicSpriteEntity
         setSpeed(speed);
     }
 
-    public void changeDirection(double direction){
+    public void changeDirection(double direction) {
         this.direction = direction;
         setDirection(direction);
         setRotate(direction);
-    };
+    }
 
-    private void changeSpriteOrientation(int direction){
+    private void changeSpriteOrientation(int direction) {
         setRotate(direction);
     }
 
-    public boolean isAlignedToGrid(){
+    public boolean isAlignedToGrid() {
         //checks if current location in scene is aligned to grid
         return ((getLocationInScene().getX() % 50) == 0) && ((getLocationInScene().getY() % 50) == 0);
     }
 
-    public Coordinate2D returnLocationInScene(){
+    public Coordinate2D returnLocationInScene() {
         return getLocationInScene();
     }
 
-    public void changeSpeed(double speed){
+    public void changeSpeed(double speed) {
         setSpeed(speed);
     }
 
+    public void handleCollision(Snake snake) {
+        if (this.getLocationInScene().distance(snake.getAnchorLocation()) < 5) {
+
+        }
+    }
 }
